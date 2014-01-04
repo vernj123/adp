61909e0d8031cca93b875f4034e96158b8e598da
vernj123 <vjacques01@gmail.com>
2014-01-04 17:59:04 -0500

    modify readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Angry Dalek Program
+
+Angry Dalek. Write an angry Dalek program that rudely asks what
+#you want. Whatever you answer, the angry boss should yell it
+#back to you and then exterminate you. 
---
+++ adp.rb
@@ -0,0 +1,13 @@
+#Angry boss. Write an angry boss program that rudely asks what
+#you want. Whatever you answer, the angry boss should yell it
+#back to you and then fire you. For example, if you type in I want
+#a raise, it should yell back.
+#I am going to do a angry dalek program instead.
+
+puts 'What is the human request?'
+request = gets.chomp
+puts 'Request '+request.upcase+' is not our protocol for humans!'
+puts 'Violation!'
+puts 'EXTERMINATE! EXTERMINATE!'
+
+
